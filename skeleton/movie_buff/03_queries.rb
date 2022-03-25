3deeeee4ac2e20b0502a4d5e7cf674228aea4504
AA Student <appacademy@Mini-10.local>
2022-03-25 16:29:57 -0700

    Complete Golden Sum

--- skeleton/movie_buff/03_queries.rb
+++ skeleton/movie_buff/03_queries.rb
@@ -1,11 +1,27 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  
+  # movie = Movie
+  #   .select(:id, :title)
+  #   .joins(:actors)
 
+
+    Movie 
+      .select(:id, :title) 
+      .joins(:actors)
+      .group(:id)
+      .having('COUNT(movies.id) = ?', those_actors.length)
+      .where(actors: {name: those_actors})
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+  
+  Movie
+    .select('(yr / 10) * 10 AS decade')
+    .group('decade')
+    .order("AVG(score) DESC").first.decade
 
 end
 
